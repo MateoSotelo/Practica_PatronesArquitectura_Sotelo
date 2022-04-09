f1dfa7666d58766a932af9dfdae8e5c011733b42
Msotelo1 <msotelo111201@gmail.com>
2022-04-08 21:19:52 -0300

    Persistencia Creada

--- Practica-Patrones/Cobertura.cs
+++ Practica-Patrones/Cobertura.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Practica_Patrones
+namespace LogicaDeNegocios
 {
     public enum tipoCobertura { Maxima, Normal};
     public class Cobertura
